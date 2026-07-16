38f1d085f60fe1e5d5aaacfd985bd53ee5fff223
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-07-16 23:33:20 +0530

    area

---
+++ day-8/area.py
@@ -0,0 +1,3 @@
+side = int(input ("Enter length of one side of a square:"))
+area = side*side
+print(area)
---
+++ day-8/floating_area.py
@@ -0,0 +1,4 @@
+a = float(input("Enter a number:"))
+b = float(input("Enter a number:"))
+avg = (a+b)/2
+print(avg)
