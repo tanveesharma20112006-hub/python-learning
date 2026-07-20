80866b04b2f833c4b3e91ca396e865627460b3cc
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-07-20 17:22:12 +0530

    remove_last_inserted

---
+++ day-12/removing_last_inserted.py
@@ -0,0 +1,9 @@
+dict={
+    "name" : "tanvee",
+    "roll.no":"240280152",
+    "batch" : "2024",
+    "subjects":["eng","hindi", "math", "punjabi","science" ]
+}
+item = dict.popitem()
+
+print(dict)
