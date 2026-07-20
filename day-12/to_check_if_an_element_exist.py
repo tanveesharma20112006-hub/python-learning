15159ab0fbe425c7f24c2ac7deb3752532273c6c
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-07-20 17:11:10 +0530

    try and found

---
+++ day-12/to_check_if_an_element_exist.py
@@ -0,0 +1,7 @@
+subjects = ("English", "Hindi","Mathematics", "Science")
+item = input("Enter Subject:")
+if item in subjects:
+    print("FOUND!!!!")
+else:
+    print("OOPS!! TRY AGAIN LATER ,ALLIGATOR")
+          
