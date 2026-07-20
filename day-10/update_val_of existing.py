b32a0f0b545f18bc96f2453a5d583d766b1b86ae
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-07-20 16:04:56 +0530

    updating value of existing key

---
+++ day-10/update_val_of existing.py
@@ -0,0 +1,8 @@
+dict={
+    "name" : "tanvee",
+    "roll.no":"240280152",
+    "batch" : "2024",
+    "subjects":["eng","hindi", "math", "punjabi","science" ]
+}
+dict["batch"] = "2025"
+print(dict)  
