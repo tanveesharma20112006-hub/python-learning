f9176ff61db6f5efc4a4aa0531c5eea2a400ef02
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-07-22 13:25:58 +0530

    leap year checker

--- day-13/leap_year_checker.py
+++ day-13/leap_year_checker.py
@@ -1,3 +1,10 @@
+"""lll                                                                   hh                    kk                   
+lll   eee    aa aa pp pp      yy   yy   eee    aa aa rr rr       cccc hh        eee    cccc kk  kk   eee  rr rr  
+lll ee   e  aa aaa ppp  pp    yy   yy ee   e  aa aaa rrr  r    cc     hhhhhh  ee   e cc     kkkkk  ee   e rrr  r 
+lll eeeee  aa  aaa pppppp      yyyyyy eeeee  aa  aaa rr        cc     hh   hh eeeee  cc     kk kk  eeeee  rr     
+lll  eeeee  aaa aa pp              yy  eeeee  aaa aa rr         ccccc hh   hh  eeeee  ccccc kk  kk  eeeee rr     
+                   pp          yyyyy        """
+
 year = int(input("Enter year:"))
 if year % 4 == 0:
     if year % 100 == 0:
