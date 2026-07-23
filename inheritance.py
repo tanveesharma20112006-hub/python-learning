11302656c687bcef392a28efb3482df4977cacef
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-07-23 17:51:48 +0530

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,14 @@
+# it allows a class to acquire properties and methods of another class.
+class Vehicle:
+    def __init__(self,brand):
+        self.brand = brand
+    def start(self):
+        print(f"{self.brand} is starting...")
+class Bike(Vehicle):
+    def __init__(self,brand):
+        super().__init__(brand)
+    def ride(self):
+        print(f"{self.brand} is being ridden.")
+bike = Bike("Yamaha")
+bike.start()
+bike.ride()
