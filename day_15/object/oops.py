0a284fc8dcfe696bb35a30718cef0cc7213dbee5
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-07-24 12:14:07 +0530

    to create car model and brand by using classes and object

--- day_15/object/oops.py
+++ day_15/object/oops.py
@@ -1,1 +1,18 @@
-  
+# object oriented programming is a way of structuring code using classes and object to model real world entities . it help in modularity & scalability.
+#classes and object :
+#class - Blueprint for creating object
+# object - instances of
+#  class 
+class Car:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+    
+    def display_info(self):
+        print(f"Car:{self.brand}{self.model}")
+
+#creating object
+car1 = Car("Toyota","Corolla")
+car2 = Car ("Tesla","Model S")
+car1.display_info()
+car2.display_info()
