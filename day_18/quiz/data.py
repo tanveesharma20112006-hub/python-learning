5bd5052fa530604d8efaa19d3037adcd29569ac9
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-07-29 17:49:36 +0530

    quiz

---
+++ day_18/quiz/data.py
@@ -0,0 +1,22 @@
+question_data = [
+    {
+        "text": "A slug's blood is green.",
+        "answer": "True"
+    },
+    {
+        "text": "The loudest animal is the African Elephant.",
+        "answer": "False"
+    },
+    {
+        "text": "Approximately one quarter of human bones are in the feet.",
+        "answer": "True"
+    },
+    {
+        "text": "The total surface area of a human lungs is the same size as a football pitch.",
+        "answer": "True"
+    },
+    {
+        "text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.",
+        "answer": "True"
+    }
+]
