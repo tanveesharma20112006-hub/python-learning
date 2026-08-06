74c0595f7c0c05320676ca843d22c397aeef1bc7
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-08-06 12:20:33 +0530

    shapes

---
+++ day_25/screen.py
@@ -0,0 +1,11 @@
+import turtle as t
+from turtle import Screen
+shapes = input(" arrow , square , triangle, circle, classic , turtle \n")
+
+#shape = screen.textinput("arrow" , "square" , "triangle" , "circle" , "classic" , "turtle" )
+screen = Screen()
+if shapes in ["arrow","square","triangle","circle","classic","turtle"]:
+    t.shape(shapes)
+else:
+    print("invalid input")
+screen.exitonclick()
