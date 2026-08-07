6e47f598316028ba00d1c78564f591a9a62a0177
tanveesharma20112006-hub <tanveesharma.20112006@gmail.com>
2026-08-07 17:05:48 +0530

    modulo.py

---
+++ day_26/ma.py
@@ -0,0 +1,7 @@
+import numpy as np 
+arr1 = np.array ([[1,2,3],[4,5,6]])
+print("Matrix 1 :",arr1)
+arr2 = np.array([[7,8,9], [10,11,12]])
+print("Matrix 2 :",arr2)
+arr3 = arr1 % arr2
+print("Matrix 3:",arr3)
